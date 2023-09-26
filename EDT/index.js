7c8501423383ae120280a10450de7713d6cfd408
Favier Guillaume <guillaume.favier.dev@gmail.com>
2023-09-26 17:29:02 +0200

    🎨 Darkmode 🌙

--- EDT/index.js
+++ EDT/index.js
@@ -28,29 +28,6 @@ async function getText(url) {
 }
 
 
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-
 
 var selectGrp = document.getElementById("grpKhole")
 for (let i = 1; i <= 16; i++) {
--- nav/nav.js
+++ nav/nav.js
@@ -7,3 +7,48 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+if (getCookie("dark") == "") {
+    setCookie("dark", "false", 365)
+}
+
+const applyTheme = () => {
+    if (getCookie("dark") == "true") {
+        document.getElementById("genestyle").setAttribute("href", "/nightGeneral.css")
+    } else {
+        document.getElementById("genestyle").setAttribute("href", "/general.css")
+    }
+}
+
+
+document.getElementById("dark").onclick = () => {
+    if (getCookie("dark") == "true") {
+        setCookie("dark", "false", 365)
+        applyTheme()
+    }else{
+        setCookie("dark", "true", 365)
+        applyTheme()
+    }
+}
+applyTheme()
