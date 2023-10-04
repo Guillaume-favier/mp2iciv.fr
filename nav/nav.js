38d5fed6d4fa67fb7e72734de94ca428bfeccb02
Favier Guillaume <guillaume.favier.dev@gmail.com>
2023-10-04 15:51:36 +0200

    🎨 whhaaa le darkmode trop stylé de fou

--- nav/nav.js
+++ nav/nav.js
@@ -1,3 +1,6 @@
+document.getElementById("url").innerText = window.location.hostname
+
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
