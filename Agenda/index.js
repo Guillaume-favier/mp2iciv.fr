d386fbf357ee7717f1ed3e93a45f8e7d4cd40113
Favier Guillaume <guillaume.favier.dev@gmail.com>
2023-10-08 12:46:20 +0200

    🪛 c'est moche les tables

--- Agenda/index.js
+++ Agenda/index.js
@@ -44,34 +44,27 @@ async function getJson(url) {
     })
 
     const Bdiv = document.getElementById("contrib")
-    const table = Bdiv.children[1]
-    const pfpGithub = document.createElement("tr")
-    const nameGithub = document.createElement("tr")
-    const nbCommit = document.createElement("tr")
+    const table = document.getElementById("listContr")
     let contrib = await getJson("https://api.github.com/repos/Guillaume-favier/agendaMP2I/contributors")
     contrib.forEach(e => {
-        const pfp = document.createElement("td")
-        const name = document.createElement("td")
-        const nb = document.createElement("td")
+        const div = document.createElement("div")
         const a = document.createElement("a")
         a.href = e.html_url
         const a2 = a.cloneNode(true);
         a.innerHTML = `<img src="${e.avatar_url}" alt="pfp" width="100" height="100">`
-        pfp.appendChild(a)
 
         
 
         a2.innerText = e.login
-        name.appendChild(a2)
+        const nb = document.createElement("p")
         nb.innerText = e.contributions == 1 ? e.contributions + " contribution" : e.contributions + " contributions"
 
-        pfpGithub.appendChild(pfp)
-        nameGithub.appendChild(name)
-        nbCommit.appendChild(nb)
+        div.appendChild(a)
+        div.appendChild(document.createElement("br"))
+        div.appendChild(a2)
+        div.appendChild(nb)
+        table.appendChild(div)
     })
-    table.appendChild(pfpGithub)
-    table.appendChild(nameGithub)
-    table.appendChild(nbCommit) 
     
 
     
