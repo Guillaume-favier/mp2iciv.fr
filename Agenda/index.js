e7fda2d45a3a9ea392c1bc916485b72352a5bf03
Favier Guillaume <guillaume.favier.dev@gmail.com>
2023-10-08 11:24:19 +0200

    🔨 reconnaissance ...

--- Agenda/index.js
+++ Agenda/index.js
@@ -3,7 +3,11 @@ async function getText(url) {
     const txt = await response.text();
     return txt
 }
-
+async function getJson(url) {
+    const response = await fetch(url);
+    const jj = await response.json();
+    return jj
+}
 
 ;(async() => {
     let generalText = getText("https://guillaume-favier.github.io/agendaMP2I/agenda.txt");
@@ -39,6 +43,35 @@ async function getText(url) {
         document.getElementsByClassName("content")[1].appendChild(big)
     })
 
+    const Bdiv = document.getElementById("contrib")
+    const table = Bdiv.children[1]
+    const pfpGithub = document.createElement("tr")
+    const nameGithub = document.createElement("tr")
+    const nbCommit = document.createElement("tr")
+    let contrib = await getJson("https://api.github.com/repos/Guillaume-favier/agendaMP2I/contributors")
+    contrib.forEach(e => {
+        const pfp = document.createElement("td")
+        const name = document.createElement("td")
+        const nb = document.createElement("td")
+        const a = document.createElement("a")
+        a.href = e.html_url
+        const a2 = a.cloneNode(true);
+        a.innerHTML = `<img src="${e.avatar_url}" alt="pfp" width="100" height="100">`
+        pfp.appendChild(a)
+
+        
+
+        a2.innerText = e.login
+        name.appendChild(a2)
+        nb.innerText = e.contributions + " contributions"
+
+        pfpGithub.appendChild(pfp)
+        nameGithub.appendChild(name)
+        nbCommit.appendChild(nb)
+    })
+    table.appendChild(pfpGithub)
+    table.appendChild(nameGithub)
+    table.appendChild(nbCommit) 
     
 
     
