8b085d8cd9cd0abe4f67e84287f365250927aecc
Guillaume Favier <49278215+Guillaume-favier@users.noreply.github.com>
2023-10-21 10:42:14 +0200

    🎉 bonne vacances

--- EDT/index.js
+++ EDT/index.js
@@ -165,7 +165,7 @@ const ajusteDate = (n) => {
 
     let groupeK = selectGrp.value == "" ? 0 : Number(selectGrp.value);
     let groupeI = 0;
-    let semaine = 7;
+    let semaine = 8;
     semaines.value = semaine
     let CKh = 0;
     let kholes = [""]
