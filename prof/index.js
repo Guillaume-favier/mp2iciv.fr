4be1e5571e183cb35159a1b1be846b4d987b1139
Guillaume Favier <49278215+Guillaume-favier@users.noreply.github.com>
2023-12-16 12:05:02 +0100

    Update index.js

--- prof/index.js
+++ prof/index.js
@@ -42,7 +42,7 @@ const getGrpFromSem = (s) => {
     const semaineNom = await getJson("/EDT/semaine.json")
     const groupes = await getJson("/EDT/groupes.json")
 
-    let currSem = 13;
+    let currSem = 14;
     semaines.value = currSem
 
     const oneGrp = (s,c) => {
